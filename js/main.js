46e807d79aada65aaf241c2f9aa451ef44e9e7f2
okcompte <afternext.ltd@gmail.com>
2022-01-01 23:12:09 +0100

    a

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
